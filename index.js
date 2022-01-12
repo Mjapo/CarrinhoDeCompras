749e6e7b636c81c3e951dd35db003f171c3d9ed7
Mjapo <marceloishida14@gmail.com>
2022-01-11 21:42:04 -0300

    segundo commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function add() {
+var v = document.getElementById("num");
+v.innerHTML = parseInt(v.innerHTML) + 1;
+
+}
+
+function one() {
+var x = document.getElementById("num-two");
+x.innerHTML = parseInt(x.innerHTML) + 1;
+}
+
+
+function tree() {
+var y = document.getElementById("num-tree");
+y.innerHTML = parseInt(y.innerHTML) + 1;
+}
